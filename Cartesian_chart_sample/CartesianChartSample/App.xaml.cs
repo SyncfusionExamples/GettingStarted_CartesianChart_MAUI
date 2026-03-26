7a680c9057b76ed1adfd745ec6ab2c850822349f
Kamalesh-Periyasamy <Kamalesh.periyasamy@syncfusion.com>
2026-03-26 12:11:36 +0530

    Added new sample

---
+++ Cartesian_chart_sample/CartesianChartSample/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CartesianChartSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Cartesian_chart_sample/CartesianChartSample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CartesianChartSample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
